0dd5ce3a3922a62f931f3331d8fa2a1b7ab4eb70
nx7400 <michaszek17@gmial.com>
2016-10-20 22:54:14 +0200

    Simple registration form

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on October 14, 2016, 10:01 pm.
---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/registration")
+     */
+    public function registrationAction(Request $request)
+    {
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            //return $this->redirectToRoute('replace_with_some_route');
+            return new Response('Saved new user with id '.$user->getId());
+        }
+
+        return $this->render('AppBundle:Registration:registration.html.twig', array('form' => $form->createView()
+        ));
+    }
+
+}
